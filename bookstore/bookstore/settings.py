3270c417de67fdf802b0fdb67fa85f25a9659ded
ASWINDAS <cvaswin1997@gmail.com>
2023-11-09 11:05:13 +0530

    add some changes

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h$n&-uy$r-8fln5awh^mk6l90t&b^5(0%2#9n7xp-@3eju^af'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
